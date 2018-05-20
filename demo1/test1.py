49947752dc0a7962bae75c7ffa4f9dd87bfdcc4b
KyleWang <wk52525@126.com>
2018-05-20 15:52:55 +0800

    多个变量打印

--- demo1/test1.py
+++ demo1/test1.py
@@ -23,10 +23,11 @@ money = applePrice + weight
 money = money - 10
 
 # 输入输出(注意python2和python3输入的区别)
-
 name = input("请输入你的名字:")
 print("姓名是" + name)
 age = 18
 print("age变量里的值是%d" % age)  # %d:整数占位符
 gender = "男"
 print("gender变量里的值是%s" % gender)  # %s:字符串占位符
+print("姓名是:%s,年龄是:%d,性别是:%s" % (name, age, gender))  # 多个占位符
+
