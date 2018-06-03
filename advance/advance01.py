19109de814506a5c7e59959bcbd65e5726307329
KyleWang <wk52525@126.com>
2018-06-03 19:51:25 +0800

    元组

--- advance/advance01.py
+++ advance/advance01.py
@@ -24,3 +24,19 @@ print(L[2])
 print(L[-2])
 print(L[1:])
 
+# 元组
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = ()
+tup3 = (50,)
+
+print(tup1[0])
+
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
+# 修改元组非法,可连接创建新元组
+tup3 = tup1 + tup2
+print(tup3)
+# 元组不允许删除,可del删除整个元组
+tup1 = ('physics', 'chemistry', 1997, 2000)
+print(tup1)
+del tup1
