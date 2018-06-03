9a3b70fa115cbae84252f1d43f55ccdc9d9eb960
KyleWang <wk52525@126.com>
2018-06-03 19:44:21 +0800

    列表

---
+++ advance/advance01.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# @TIME     : 20180603 19:30
+# @Author   : KyleWang
+# @File     : advance01.py
+
+
+# 列表
+myList = ['physics', 'chemistry', 1997, 2000]
+print(myList[0])
+
+print(myList)
+del myList[0]
+print(myList)
+
+print(len([1, 2, 3]))
+print([1, 2, 3] + [4, 5, 6])
+print(['Hi!' * 4])
+print(3 in [1, 2, 3])
+for x in [1, 2, 3]:
+    print(x)
+
+L = ['hello', 'Hello', 'HELLO!']
+print(L[2])
+print(L[-2])
+print(L[1:])
+
