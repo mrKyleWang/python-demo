901002ec70d81ae7b7726f097b8ec05ac0195216
KyleWang <wk52525@126.com>
2018-06-17 15:52:00 +0800

    日期时间

--- advance/advance02.py
+++ advance/advance02.py
@@ -20,4 +20,21 @@ del dict['name']
 dict.clear()
 del dict
 
+# 日期和时间
+import time, datetime
+
+localtime = time.localtime(time.time())
+print("local current time:", localtime)
+
+print(time.strftime('%Y-%m-%d %H:%M:%S'))
+print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'))
+print(str(datetime.datetime.now())[:19])
+
+expire_time = "2018-06-17 15:37:36"
+d = datetime.datetime.strptime(expire_time, '%Y-%m-%d %H:%M:%S')
+print(d)
+
+
+
+
 
