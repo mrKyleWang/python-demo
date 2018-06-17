bdee033eb9ebab9b4c8c5b4ca11a4710cc6116c4
KyleWang <wk52525@126.com>
2018-06-17 16:46:38 +0800

    函数

---
+++ advance/advance03.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+# @TIME     : 20180617 16:34
+# @Author   : KyleWang
+# @File     : advance03.py
+
+
+lst = [1, 2, 1, 1, 2, 3, 4, 2, 1, 2, 5, 6, 1, 2]
+
+
+def statistics(lst):
+    dic = {}
+    for k in lst:
+        if not k in dic:
+            dic[k] = 1
+        else:
+            dic[k] += 1
+    return dic
+
+
+print(statistics(lst))
+
+
+def statistics2(lst):
+    m = set(lst)
+    dic = {}
+    for x in m:
+        dic[x] = lst.count(x)
+    return dic
+
+
+print(statistics2(lst))
